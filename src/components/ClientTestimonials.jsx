a445093239b386b8c79b0eef7b36a047e34a2590
sangeeth-606 <sangeeth999123@gmail.com>
2025-02-24 09:45:14 +0000

    added

---
+++ src/components/ClientTestimonials.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+// import React from 'react'
+
+function ClientTestimonials({name ,rev}) {
+  return (
+    <div>
+        <h1>{name}</h1>
+        <h2>{rev}</h2>
+    </div>
+  )
+}
+
+export default ClientTestimonials
